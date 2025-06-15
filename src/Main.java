67a370ba6177700e805583d624030d1ea5c4cb07
lawci <lcft1204@gmail.com>
2025-06-15 22:11:11 +0800

    init commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double balance = 5000.00;
+        double interestRate = 0.17;
+        double month1Interest = balance * interestRate;
+        double month2Interest = (balance + month1Interest) * interestRate;
+        System.out.println("Month 1 Interest: $" + month1Interest);
+        System.out.println("Month 2 Interest: $" + month2Interest);
+    }
+}
